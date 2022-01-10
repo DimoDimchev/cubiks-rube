2d3b15f6c87efbb9c500d9d5c1a91075ac8088c1
Dimo Dimchev <ddimchev7@gmail.com>
2022-01-10 21:34:14 +0200

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import * as React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import { HomePage, AboutPage } from "./components/pages";
-import "./App.css";
+import "./styles/main.scss";
 
 export default function App() {
 	return (
 		<div className="App">
-			<h1>Welcome to React Router!</h1>
+			<div className="nav">
+				<Link className="link" to="/">
+					Home
+				</Link>
+				<Link className="link" to="/about">
+					About
+				</Link>
+			</div>
+
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="about" element={<AboutPage />} />
--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,5 +1,11 @@
 import React from "react";
+import "../../styles/main.scss";
 
 export default function HomePage() {
-	return <h1>Home</h1>;
+	return (
+		<div>
+			<h1>Welcome to Cubik's Rube!</h1>
+			<p>The average Rubik's Cube enjoyer's playground</p>
+		</div>
+	);
 }
